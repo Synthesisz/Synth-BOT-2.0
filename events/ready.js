9134809f648bd978da0cc0ed0a19f5c507b0ef10
Synthesisz <84045671+Synthesisz@users.noreply.github.com>
2023-12-21 01:54:18 +0200

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,10 @@
-
+// Desc: Event that is triggered when the bot is ready
+const {Events, ActivityType} = require('discord.js');
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    console.log(`Ready! Logged in as ${client.user.tag}`);
+    client.user.setActivity('Demon Hunter', {type: ActivityType.PLAYING});
+  },
+};
