93c1fb3f09ef85208b119151eadf086da5da0907
Synthesisz <84045671+Synthesisz@users.noreply.github.com>
2023-12-21 02:33:19 +0200

    Add files via upload

--- utility/fetch-quote.js
+++ utility/fetch-quote.js
@@ -1,1 +1,8 @@
+const fetch = require('isomorphic-unfetch');
 
+module.exports.getQuote = async endpoint => {
+  const data = await fetch(`https://animechan.xyz/api/${endpoint}`);
+  const response = await data.json();
+
+  return response;
+};
